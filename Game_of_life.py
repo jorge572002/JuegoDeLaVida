984cd3d38200f7d7dd67205e499ff84bd35a5c03
Jorge <jorge572002@hotmail.com>
2024-06-23 15:21:36 -0600

    Escritura de instrucciones

---
+++ Game_of_life.py
@@ -0,0 +1,10 @@
+"""En este coódigo se desarrolla el famoso algoritmo el juego de la vida
+creado por el matematico John Conway. Las reglas son simples
+Para sectores poblados:
+1.Una celula con 1 o 0 vecinos muere de soledad
+2.Una celula con 4 o mas vecinos muero por sobrepoblación
+3.Una celula con 2 o 3 vecinos vive
+Para sectores no poblados:
+1. Al haber 3 vecinos, una celula nace
+"""
+
